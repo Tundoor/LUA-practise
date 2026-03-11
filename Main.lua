c8a7c0773fa087a7c3339d48a8f3a80b001301c0
Thandolwethu Mbokazi <thandwem045@gmail.com>
2026-03-11 14:07:27 +0000

    Added for loop implementation testing

--- Main.lua
+++ Main.lua
@@ -30,3 +30,57 @@ else
 end
 
 -- Loops in lua are kinda complex...
+-- How loops are working 
+
+--[[
+
+While Loops
+A while loop is a loop that will always run as long it has a condition that is true. If the condition is set to false or nil, it will not loop at all. Everything that comes after a while, except false and nil, will always be seen as true.
+
+The loop will always check that its condition is a true value before executing.
+
+while { condition } do
+   statement(s)
+end
+
+For Loops
+A for loop is a loop that allows us to execute something a specific amount of times. For example, we can use a for loop to loop through a series of numbers or connected players in a server.
+
+The syntax for creating a for loop is the following:
+
+for init,max/min value, increment do
+   statement(s)
+end
+
+Repeat…until Loop
+A repeat loop is a loop that checks its condition at the bottom of the loop, unlike a while loop, which checks it at the top.
+
+It’s very similar to the while loop, but it’s not guaranteed to loop with its condition, unlike with the while loop.
+
+The syntax for a repeat loop is the following:
+
+repeat
+   statement(s)
+until { condition }
+
+If you for some reason need to break a while loop, you can add this to break the loop after a condition a met. For example, I like to add a break if a condition with an if statement changes.
+
+To implement a break, you’d do this:
+
+while true do
+   if Action == false then
+      -- // Repeat the loop
+   else
+      -- // Break the loop
+      break
+   end
+end
+
+]]
+
+local name = "Thandolwethu"
+
+-- We start at 1 because lau is 1 indexed, not 0 indexed
+for i = 1, #name, 1 do
+  print("Thando")
+end
