8e96dc6ff059715ca0722246aff91bcc420ebeaf
Thandolwethu Mbokazi <thandwem045@gmail.com>
2026-03-24 11:24:40 +0000

    type casting

--- Main.lua
+++ Main.lua
@@ -170,3 +170,32 @@ var = io.read()
 print("Hello " .. var)
 
 -- The input is always of type string. For example, if the input is 56 then var will hold the string "56".
+
+-- ####################################### Type casting 
+
+-- To convert the input to a different type, we need to cast.
+
+-- To cast a string to a number, we will write:
+
+var = io.read()
+var = tonumber(var)
+
+-- Or in one line,
+
+var = tonumber(io.read())
+-- If the input is a number, i.e. 5, 4, 54 then var will hold a number. If the input contains an invalid number: 5ab, bb, akt, etc. then tonumber() will return nil.
+
+-- Here is a table that shows how to cast to different types:
+
+-- Cast	Explanation
+-- tonumber()	Convert to a number
+-- tostring()	Convert to a string
+-- It is important to use the right type because it can affect the output.
+
+-- Concatenating two strings will result in:
+
+-- "5" .. "5" = "55"
+
+-- Adding two numbers will result in:
+
+-- 5 + 5 = 10
